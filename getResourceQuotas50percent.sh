afeb044c11f01024ff6eb30ffe9dfe2859c7d39e
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-21 11:44:15 -0500

    Update getResourceQuotas50percent.sh

--- getResourceQuotas50percent.sh
+++ getResourceQuotas50percent.sh
@@ -3,10 +3,23 @@
 # Set the threshold for resource quota usage
 THRESHOLD=50
 
-# Run kubectl to get a list of namespaces and their resource quotas,
-# and filter out the ones that are using less than 50% of their quotas
-kubectl get namespaces -o json | \
-  jq -r '.items[] | .metadata.name as $ns | .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration" | fromjson.spec[]? | select(.kind=="ResourceQuota") | select(.metadata.namespace==$ns) | select((.status.hard | to_entries)[] | .value != null and (.status.used | to_entries)[] | .value != null and (.status.used | to_entries)[] | .value / (.status.hard | to_entries)[] | .value * 100 < '$THRESHOLD') | $ns' | \
-  sort | uniq | \
-  awk 'BEGIN {FS="\n"; RS=""} {print "\"" $1 "\""}' | \
-  sed -e '1i "Namespace"' > namespace_quotas.csv
+# Get a list of namespaces in the cluster
+namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
+
+# Loop through the namespaces and check their resource quota usage
+for ns in $namespaces
+do
+    # Get the resource quota for the namespace
+    quota=$(kubectl get resourcequota -n $ns -o jsonpath='{.items[0].status.hard}')
+
+    # Get the resource usage for the namespace
+    usage=$(kubectl get resourcequota -n $ns -o jsonpath='{.items[0].status.used}')
+
+    # Calculate the percentage of resource quota usage
+    usage_percentage=$(echo "scale=2; $usage/$quota*100" | bc)
+
+    # Check if the usage percentage is less than the threshold
+    if (( $(echo "$usage_percentage < $THRESHOLD" | bc -l) )); then
+        echo "$ns is using $usage_percentage% of its resource quota"
+    fi
+done

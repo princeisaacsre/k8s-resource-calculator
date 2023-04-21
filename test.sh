e76f3fb44d9e9a68438ec41051104f01f9c207f1
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-21 12:25:09 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 output_file="resource_quotas.csv"
 
 # Write the CSV header
-echo "Namespace,Quota Name,AGE (days),JOBS,CPU Limits,CPU Requests,Memory Limits,Memory Requests" > $output_file
+echo "Namespace,Quota Name,AGE (days),JOBS,CPU Limits,Memory Limits,CPU Requests,Memory Requests" > $output_file
 
 # Get the list of namespaces
 namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
@@ -21,8 +21,8 @@ for namespace in $namespaces; do
       .metadata.name,
       (($now - (.metadata.creationTimestamp | sub("\\.[0-9]+Z"; "Z") | fromdate)) / 86400 | floor),
       (.spec.hard."count.jobs.batch" // "N/A"),
-      (.spec.hard."limits.cpu" // "N/A"), (.spec.hard."requests.cpu" // "N/A"),
-      (.spec.hard."limits.memory" // "N/A"), (.spec.hard."requests.memory" // "N/A")
+      (.spec.hard."limits.cpu" // "N/A"), (.spec.hard."limits.memory" // "N/A"),
+      (.spec.hard."requests.cpu" // "N/A"), (.spec.hard."requests.memory" // "N/A")
       ] | @csv' >> $output_file
   fi
 done

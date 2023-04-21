f7ba490d366e50972f26a698aaa403fd06758839
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-21 12:21:56 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,18 +4,10 @@
 output_file="resource_quotas.csv"
 
 # Write the CSV header
-echo "Namespace,Quota Name,Age (days),Job Count,CPU Limit,Memory Limit" > $output_file
+echo "Namespace,Quota Name,AGE (days),JOBS,CPU Limits,CPU Requests,Memory Limits,Memory Requests" > $output_file
 
 # Get the list of namespaces
 namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
-
-# Function to calculate the age in days
-get_age_in_days() {
-  local creation_timestamp=$1
-  local current_date=$(date +%s)
-  local creation_date=$(date -d $creation_timestamp +%s)
-  echo $(( (current_date - creation_date) / 86400 ))
-}
 
 # Iterate through each namespace and extract resource quotas
 for namespace in $namespaces; do
@@ -29,8 +21,8 @@ for namespace in $namespaces; do
       .metadata.name,
       (($now - (.metadata.creationTimestamp | sub("\\.[0-9]+Z"; "Z") | fromdate)) / 86400 | floor),
       (.spec.hard."count.jobs.batch" // "N/A"),
-      (.spec.hard."limits.cpu" // "N/A"),
-      (.spec.hard."limits.memory" // "N/A")
+      (.spec.hard."limits.cpu" // "N/A"), (.spec.hard."requests.cpu" // "N/A"),
+      (.spec.hard."limits.memory" // "N/A"), (.spec.hard."requests.memory" // "N/A")
       ] | @csv' >> $output_file
   fi
 done

4aebd7aef5164d21b3de1f0f8f8d6e8006138d20
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-21 12:02:41 -0500

    Update getResourceQuotas50percent.sh

--- getResourceQuotas50percent.sh
+++ getResourceQuotas50percent.sh
@@ -4,22 +4,21 @@
 namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
 
 # Create a header row for the CSV file
-echo "Namespace,Resource,Hard,Used" > resource_quotas.csv
+echo "Namespace,CPU Used (%),Memory Used (%)" > resource_quota_usage.csv
 
-# Loop through the namespaces and export their resource quotas to the CSV file
+# Loop through the namespaces and export their resource quota usage to the CSV file
 for ns in $namespaces
 do
-    # Get the resource quotas for the namespace
-    quotas=$(kubectl get resourcequota -n $ns -o jsonpath='{.items[*].metadata.name}')
+    # Get the CPU usage percentage for the namespace
+    cpu=$(kubectl top pods -n $ns --no-headers | awk '{ sum += $2 } END { printf "%.2f", sum }')
+    cpu_quota=$(kubectl get resourcequota -n $ns -o jsonpath='{.items[*].spec.hard.cpu}')
+    cpu_usage_percentage=$(echo "scale=2; $cpu / $cpu_quota * 100" | bc)
 
-    # Loop through the resource quotas and export their usage to the CSV file
-    for quota in $quotas
-    do
-        # Get the hard and used limits for the resource quota
-        hard=$(kubectl get resourcequota $quota -n $ns -o jsonpath='{.status.hard}')
-        used=$(kubectl get resourcequota $quota -n $ns -o jsonpath='{.status.used}')
+    # Get the memory usage percentage for the namespace
+    memory=$(kubectl top pods -n $ns --no-headers | awk '{ sum += $3 } END { printf "%.2f", sum }')
+    memory_quota=$(kubectl get resourcequota -n $ns -o jsonpath='{.items[*].spec.hard.memory}')
+    memory_usage_percentage=$(echo "scale=2; $memory / $memory_quota * 100" | bc)
 
-        # Add a row to the CSV file with the namespace, resource, hard limit, and used limit
-        echo "$ns,$quota,$hard,$used" >> resource_quotas.csv
-    done
+    # Add a row to the CSV file with the namespace, CPU usage percentage, and memory usage percentage
+    echo "$ns,$cpu_usage_percentage,$memory_usage_percentage" >> resource_quota_usage.csv
 done

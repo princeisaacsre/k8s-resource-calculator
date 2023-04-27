9261d3277a02ef81f8cbbac875eb462a48b5edc3
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-26 21:06:29 -0500

    Update getResourceQuotas.sh

--- single/getResourceQuotas.sh
+++ single/getResourceQuotas.sh
@@ -7,9 +7,9 @@ NAMESPACE="<your_namespace_here>"
 OUTPUT_FILE="namespace_limits.csv"
 
 # Write the CSV header to the output file
-echo "Name,Age,Request(Jobs.batch),Request(Count),Limit(limits.cpu),Limit(limits.memory)" > "${OUTPUT_FILE}"
+echo "Name,Age,Request(Jobs.batch)-Hard,Request(Jobs.batch)-Used,Request(Count)-Hard,Request(Count)-Used,Limit(limits.cpu)-Hard,Limit(limits.cpu)-Used,Limit(limits.memory)-Hard,Limit(limits.memory)-Used" > "${OUTPUT_FILE}"
 
 # Get the resource quotas and parse the output
 kubectl get resourcequota --namespace "${NAMESPACE}" -o json |
-  jq -r '.items[] | [.metadata.name, (.metadata.creationTimestamp | fromdate | strftime("%Y-%m-%d")), (.spec.hard["count/jobs.batch"] // "N/A"), (.spec.hard["requests.cpu"] // "N/A"), (.spec.hard["limits.cpu"] // "N/A"), (.spec.hard["limits.memory"] // "N/A")] | @csv' |
+  jq -r '.items[] | [.metadata.name, (.metadata.creationTimestamp | fromdate | strftime("%Y-%m-%d")), (.spec.hard["count/jobs.batch"] // "N/A"), (.status.used["count/jobs.batch"] // "N/A"), (.spec.hard["requests.cpu"] // "N/A"), (.status.used["requests.cpu"] // "N/A"), (.spec.hard["limits.cpu"] // "N/A"), (.status.used["limits.cpu"] // "N/A"), (.spec.hard["limits.memory"] // "N/A"), (.status.used["limits.memory"] // "N/A")] | @csv' |
   sed 's/"//g' >> "${OUTPUT_FILE}"

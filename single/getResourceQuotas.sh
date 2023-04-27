69288048da113447e69cadbf65051543eb30c4e6
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-26 21:20:54 -0500

    Update getResourceQuotas.sh

--- single/getResourceQuotas.sh
+++ single/getResourceQuotas.sh
@@ -4,13 +4,12 @@
 NAMESPACE="<your_namespace_here>"
 
 # Set the output CSV file
-OUTPUT_FILE="resource_quota_attributes.csv"
+OUTPUT_FILE="resource_quota.csv"
 
 # Write the CSV header to the output file
-echo "Name,Namespace,Created,Resource,Used,Hard" > "${OUTPUT_FILE}"
+echo "Name,Namespace,Age,Resource,Request,Limit" > "${OUTPUT_FILE}"
 
 # Get the resource quotas and parse the output
 kubectl get resourcequota --namespace "${NAMESPACE}" -o json |
-  jq -r '.items[] | .metadata.namespace as $ns | .metadata.name as $name | .metadata.creationTimestamp as $created | .status.used as $used | .spec.hard as $hard | ($used | to_entries[] | map({(.key): .value}) | add) as $usedMap | ($hard | to_entries[] | map({(.key): .value}) | add) as $hardMap | [$name, $ns, ($created | fromdate | strftime("%Y-%m-%d")), ($usedMap + $hardMap) | to_entries[]] | @csv' |
-  awk -F, 'BEGIN{OFS=","} {print $1,$2,$3,$4,$5,$7}' |
+  jq -r '.items[] | .metadata.namespace as $ns | .metadata.name as $name | .metadata.creationTimestamp as $created | .spec.hard as $hard | ($hard | to_entries[] | select(.key | startswith("requests.") or startswith("limits.")) ) as $entry | [$name, $ns, ($created | fromdate | strftime("%Y-%m-%d")), $entry.key, if $entry.key | startswith("requests.") then $entry.value else "N/A" end, if $entry.key | startswith("limits.") then $entry.value else "N/A" end] | @csv' |
   sed 's/"//g' >> "${OUTPUT_FILE}"

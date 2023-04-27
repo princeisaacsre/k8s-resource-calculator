05be0218158ca4e3f30783519fbe485638654251
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-26 21:03:45 -0500

    Update getResourceQuotas.sh

--- single/getResourceQuotas.sh
+++ single/getResourceQuotas.sh
@@ -4,12 +4,12 @@
 NAMESPACE="<your_namespace_here>"
 
 # Set the output CSV file
-OUTPUT_FILE="resource_quotas.csv"
+OUTPUT_FILE="namespace_limits.csv"
 
 # Write the CSV header to the output file
-echo "Namespace,Resource,Used,Hard" > "${OUTPUT_FILE}"
+echo "Name,Age,Request(Jobs.batch),Request(Count),Limit(limits.cpu),Limit(limits.memory)" > "${OUTPUT_FILE}"
 
-# Get the resource quotas, parse the output, and append it to the CSV file
+# Get the resource quotas and parse the output
 kubectl get resourcequota --namespace "${NAMESPACE}" -o json |
-  jq -r '.items[] | .metadata.namespace as $ns | .status.used | to_entries[] | [ $ns, .key, .value ] | @csv' |
+  jq -r '.items[] | [.metadata.name, (.metadata.creationTimestamp | fromdate | strftime("%Y-%m-%d")), (.spec.hard["count/jobs.batch"] // "N/A"), (.spec.hard["requests.cpu"] // "N/A"), (.spec.hard["limits.cpu"] // "N/A"), (.spec.hard["limits.memory"] // "N/A")] | @csv' |
   sed 's/"//g' >> "${OUTPUT_FILE}"

90e4291c245260c4a53dbc36fc3f6a5cc79a5420
princeisaacsre <96562456+princeisaacsre@users.noreply.github.com>
2023-04-26 21:16:19 -0500

    Update getResourceQuotas.sh

--- single/getResourceQuotas.sh
+++ single/getResourceQuotas.sh
@@ -11,5 +11,6 @@ echo "Name,Namespace,Created,Resource,Used,Hard" > "${OUTPUT_FILE}"
 
 # Get the resource quotas and parse the output
 kubectl get resourcequota --namespace "${NAMESPACE}" -o json |
-  jq -r '.items[] | .metadata.namespace as $ns | .metadata.name as $name | .metadata.creationTimestamp as $created | .status.used as $used | .spec.hard as $hard | ($used | to_entries[]) as $usedEntry | ($hard | to_entries[] | select(.key == $usedEntry.key)) as $hardEntry | [$name, $ns, ($created | fromdate | strftime("%Y-%m-%d")), $usedEntry.key, $usedEntry.value, $hardEntry.value] | @csv' |
+  jq -r '.items[] | .metadata.namespace as $ns | .metadata.name as $name | .metadata.creationTimestamp as $created | .status.used as $used | .spec.hard as $hard | ($used | to_entries[] | map({(.key): .value}) | add) as $usedMap | ($hard | to_entries[] | map({(.key): .value}) | add) as $hardMap | [$name, $ns, ($created | fromdate | strftime("%Y-%m-%d")), ($usedMap + $hardMap) | to_entries[]] | @csv' |
+  awk -F, 'BEGIN{OFS=","} {print $1,$2,$3,$4,$5,$7}' |
   sed 's/"//g' >> "${OUTPUT_FILE}"
